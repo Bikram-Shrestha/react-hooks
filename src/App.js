d1413a2048f4fc3c4bb59c06abe221bdd61c2f30
Bikram Shrestha <bikramshrestha777@gmail.com>
2019-09-08 10:46:52 +1000

    Basic Travis Setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const[userQuery,setUserQuery] = useState('');
+
+  const updateUserQuery = event => {
+    console.log('userQuery', userQuery);
+    setUserQuery(event.target.value);
+  }
+
   return (
     <div className="App">
       <h1>
-        This is just the test
+        Hello
       </h1>
+      <div className='form'>
+        <input value = {userQuery} onChange={updateUserQuery} />
+        <button>Search</button>
+      </div>
     </div>
   );
 }
